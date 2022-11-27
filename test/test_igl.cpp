87a7d0aaa1e52406127a30ca3bb1cf6f5d9058e0
zhang-zh <1563833973@qq.com>
2022-11-27 22:14:42 +0800

    update test_igl

--- test/test_igl.cpp
+++ test/test_igl.cpp
@@ -1,34 +1,14 @@
 #include <igl/opengl/glfw/Viewer.h>
+#include <igl/readOFF.h>
 
 int main(int argc, char *argv[])
 {
-  // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
-    0.0,0.0,0.0,
-    0.0,0.0,1.0,
-    0.0,1.0,0.0,
-    0.0,1.0,1.0,
-    1.0,0.0,0.0,
-    1.0,0.0,1.0,
-    1.0,1.0,0.0,
-    1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-    0,6,4,
-    0,2,6,
-    0,3,2,
-    0,1,3,
-    2,7,6,
-    2,3,7,
-    4,6,7,
-    4,7,5,
-    0,4,5,
-    0,5,1,
-    1,5,7,
-    1,7,3).finished();
+    Eigen::MatrixXd V;
+    Eigen::MatrixXi F;
+    igl::readOFF("../test/examples/bunny.off", V,F);
+    igl::opengl::glfw::Viewer viewer;
+    viewer.data().set_mesh(V,F);
+    viewer.launch();
 
-  // Plot the mesh
-  igl::opengl::glfw::Viewer viewer;
-  viewer.data().set_mesh(V, F);
-  viewer.data().set_face_based(true);
-  viewer.launch();
+    return 0;
 }
