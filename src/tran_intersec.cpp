300bc13bdf84b8a3e0cfc219daf991aa61e4218a
zhang-zh <1563833973@qq.com>
2022-11-27 21:47:08 +0800

    visualization

--- src/tran_intersec.cpp
+++ src/tran_intersec.cpp
@@ -1,8 +1,12 @@
 #include <glm/glm.hpp>
 #include <tinynurbs/tinynurbs.h>
 #include <timeIntegrator/timeIntegrator.h>
+#include <igl/opengl/glfw/Viewer.h>
+#include <igl/readOBJ.h>
+#include <igl/readOFF.h>
 
 #include <functional>
+#include <map>
 
 int main()
 {
@@ -64,12 +68,66 @@ int main()
     glm::vec4 U0{0, static_cast<float>(2.0/3.0), 0, static_cast<float>(2.0/3.0)};
     cRKsolver.solve(U0, ode, 0.0001);
     auto &U = cRKsolver.result_;
+    Eigen::MatrixXd addPoints(U.size(),3);
     for(int i=0; i<U.size(); ++i){
         auto P_i = tinynurbs::surfacePoint(p, U[i][0], U[i][1]);
-        std::cout << P_i[0] << " " << P_i[1] << " " << P_i[2] << " ;  ";
-        auto Q_i = tinynurbs::surfacePoint(q, U[i][2], U[i][3]);
-        std::cout << Q_i[0] << " " << Q_i[1] << " " << Q_i[2] << std::endl;
+        // std::cout << P_i[0] << " " << P_i[1] << " " << P_i[2] << " ;  ";
+        // auto Q_i = tinynurbs::surfacePoint(q, U[i][2], U[i][3]);
+        // std::cout << Q_i[0] << " " << Q_i[1] << " " << Q_i[2] << std::endl;
+        addPoints(i,0) = P_i[0];
+        addPoints(i,1) = P_i[1];
+        addPoints(i,2) = P_i[2];
     }
+
+    // std::ofstream outxtfile1("surface1.txt");
+    // std::ofstream outxtfile2("surface2.txt");
+    // for(float u=0; u<=1; u+=0.02){
+    //     for(float v=0; v<=1; v+=0.02){
+    //         auto point1 = tinynurbs::surfacePoint(p, u,v);
+    //         auto point2 = tinynurbs::surfacePoint(q, u,v);
+    //         outxtfile1 << point1[0] << " " << point1[1] << " " << point1[2] << std::endl;
+    //         outxtfile2 << point2[0] << " " << point2[1] << " " << point2[2] << std::endl;
+    //     }
+    // }
+    // outxtfile1.close();
+    // outxtfile2.close();
+
+    igl::opengl::glfw::Viewer viewer;
+    const auto names = {"surface1.obj", "surface2.obj"};
+    std::map<int, Eigen::RowVector3d> colors;
+    int last_selected = -1;
+    for(const auto & name : names){
+        viewer.load_mesh_from_file(std::string("../src/examples/") + name);
+        colors.emplace(viewer.data().id, 0.5*Eigen::RowVector3d::Random().array()+0.5);
+    }
+    viewer.callback_key_down =
+        [&](igl::opengl::glfw::Viewer &, unsigned int key, int mod)
+    {
+        if(key == GLFW_KEY_BACKSPACE){
+            int old_id = viewer.data().id;
+            if (viewer.erase_mesh(viewer.selected_data_index)){
+                colors.erase(old_id);
+                last_selected = -1;
+            }
+            return true;
+        }
+        return false;
+    };
+     viewer.callback_pre_draw =
+        [&](igl::opengl::glfw::Viewer &)
+    {
+        if (last_selected != viewer.selected_data_index){
+            for (auto &data : viewer.data_list){
+                data.set_colors(colors[data.id]);
+            }
+            viewer.data_list[viewer.selected_data_index].set_colors(Eigen::RowVector3d(0.9,0.1,0.1));
+            last_selected = viewer.selected_data_index;
+        }
+        return false;
+    };
+    viewer.data().add_points(addPoints, Eigen::RowVector3d(0, 1, 0));
+    viewer.data().point_size = 3.0;
+    viewer.launch();
 
     return 0;
 }
